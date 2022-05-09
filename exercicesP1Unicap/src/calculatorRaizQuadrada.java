d342dea1c8caf5d3067f0c8db76742b006250e0e
Álef Silva Rodrigues <79549354+AlefSRGS@users.noreply.github.com>
2022-05-09 09:48:33 -0300

    Create calculatorRaizQuadrada.java

---
+++ exercicesP1Unicap/src/calculatorRaizQuadrada.java
@@ -0,0 +1,19 @@
+import Java.Util.Scanner;
+public class calculatorRaizQuadrada{
+  public static void main(final String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double s = sc.nextDouble();
+		double sqrt = sqrt(s);
+		System.out.printf("%.4f\n",sqrt);
+		sc.close();
+	}
+	public static double sqrt(double c) {
+			double x1 = c; 
+			double e = c;
+			while(e > 0.00001){
+				e = Math.abs(x1 -(c/x1));
+				x1 = (x1 + c/x1)/2;
+			}
+			return x1;
+		}
+}
