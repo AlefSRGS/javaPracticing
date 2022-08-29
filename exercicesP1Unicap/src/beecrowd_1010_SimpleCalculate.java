fe370a89184ff7b48f17cf867fd4847510db853c
Alef Rodrigues <alefsrodrigues03@gmail.com>
2022-08-29 19:09:28 +0000

    praticing with beecrowd

---
+++ exercicesP1Unicap/src/beecrowd_1010_SimpleCalculate.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class beecrowd_1010_SimpleCalculate {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int numberProduct1 = sc.nextInt();
+            int numberUnitProduct1 = sc.nextInt();
+            double priceUnitProduct1 = sc.nextDouble();
+        while(sc.hasNext()){
+            int numberProduct2 = sc.nextInt();
+            int numberUnitProduct2 = sc.nextInt();
+            double priceUnitProduct2 = sc.nextDouble();
+            double totalValue = numberUnitProduct1*priceUnitProduct1 + numberUnitProduct2*priceUnitProduct2;
+            System.out.printf("VALOR A PAGAR: R$ %.2f\n",totalValue);
+        }
+        }
+}
+}
