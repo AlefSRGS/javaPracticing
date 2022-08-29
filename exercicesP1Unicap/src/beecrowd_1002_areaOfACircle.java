0fd072a36b7e85db25b78e85205cb1416b54b469
Álef Silva Rodrigues <79549354+AlefSRGS@users.noreply.github.com>
2022-08-29 10:15:35 -0300

    Update beecrowd_1002_areaOfACircle.java

--- exercicesP1Unicap/src/beecrowd_1002_areaOfACircle.java
+++ exercicesP1Unicap/src/beecrowd_1002_areaOfACircle.java
@@ -4,7 +4,7 @@ public class beecrowd_1002_areaOfACircle {
         Scanner sc = new Scanner(System.in);
 		double raio = sc.nextDouble();
 		double area = 3.14159 * (raio * raio);
-		System.out.printf("A=%.4f",area);
+		System.out.printf("A=%.4f\n",area);
 		sc.close();
     }
 }
