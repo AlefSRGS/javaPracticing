46fc1c660945ea67f15ad582feaa2dcac924858d
Álef Silva Rodrigues <79549354+AlefSRGS@users.noreply.github.com>
2022-08-29 11:01:23 -0300

    Update beecrowd_1015_DistanceBetweenTwoPoints.java

--- exercicesP1Unicap/src/beecrowd_1015_DistanceBetweenTwoPoints.java
+++ exercicesP1Unicap/src/beecrowd_1015_DistanceBetweenTwoPoints.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
 public class beecrowd_1015_DistanceBetweenTwoPoints {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        double x1 = sc.nextInt();
-        double y1 = sc.nextInt();
-        double x2 = sc.nextInt();
-        double y2 = sc.nextInt();
-        double distanceBetweenPoints = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-        System.out.printf("%.4f",distanceBetweenPoints);
-        sc.close();
+    Scanner sc = new Scanner(System.in);
+    while(sc.hasNextDouble()){
+        double x1 = sc.nextDouble();
+        double y1 = sc.nextDouble();
+    while(sc.hasNextDouble()){
+        double x2 = sc.nextDouble();
+        double y2 = sc.nextDouble();
+        double distance = Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+        System.out.printf("%.4f\n",distance);
+    }
+    }
+    sc.close();
     }
 }
