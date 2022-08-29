aefcd7fcd93b112b3a14ac121350f0670d8ca334
Álef Silva Rodrigues <79549354+AlefSRGS@users.noreply.github.com>
2022-08-29 10:46:06 -0300

    Update beecrowd_1013_TheGreatest.java

--- exercicesP1Unicap/src/beecrowd_1013_TheGreatest.java
+++ exercicesP1Unicap/src/beecrowd_1013_TheGreatest.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 public class beecrowd_1013_TheGreatest {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		int maiorAB = (a+ b +Math.abs(a-b));
-		int maiorABC = (maiorAB+ c +Math.abs(maiorAB-c));
-		System.out.println(maiorABC+" eh o maior");
-		sc.close();
+        while(sc.hasNextInt()){
+        int a = sc.nextInt(); 
+	int b = sc.nextInt();
+	int c = sc.nextInt();
+        int maiorAB = (a+b+Math.abs(a-b))/2;
+        int maiorABC = (maiorAB+c+Math.abs(maiorAB-c))/2;
+        System.out.printf("%d eh o maior\n",maiorABC);
+    }
+        sc.close();
     }
 }
