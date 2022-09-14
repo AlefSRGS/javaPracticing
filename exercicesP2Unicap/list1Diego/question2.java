437a8c1da19fb469349cad1e0734d44af96ef2a4
Alef Rodrigues <alefsrodrigues03@gmail.com>
2022-09-14 04:52:58 +0000

    unfinished question 2 list1 diego

---
+++ exercicesP2Unicap/list1Diego/question2.java
@@ -0,0 +1,17 @@
+package exercicesP2Unicap.list1Diego;
+public class question2 {
+    class Volume {
+            static int getName(int volume){
+                if(volume <= 0){
+                    String result = "Min Volume";
+                    return result;
+                }
+                if(volume > 0 && volume < 25){
+                    System.out.println("Low Volume");
+                }
+            }
+        }
+    public static void main(String[] args) throws Exception{
+        
+    }
+}
