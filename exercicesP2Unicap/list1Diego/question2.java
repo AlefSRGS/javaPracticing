4b818936bc1c2ec1019a0bade305a4b926989bf0
Alef Rodrigues <alefsrodrigues03@gmail.com>
2022-09-14 14:03:40 +0000

    list 1 diego complete

--- exercicesP2Unicap/list1Diego/question2.java
+++ exercicesP2Unicap/list1Diego/question2.java
@@ -1,17 +1,31 @@
 package exercicesP2Unicap.list1Diego;
 public class question2 {
     class Volume {
-            static int getName(int volume){
+            static String getName(int volume){
+                String result;
                 if(volume <= 0){
-                    String result = "Min Volume";
-                    return result;
-                }
-                if(volume > 0 && volume < 25){
-                    System.out.println("Low Volume");
+                    return result = "Min Volume";
+
+                }else if(volume > 0 && volume <= 25){
+                    return result = "Low Volume";
+
+                }else if(volume > 25 && volume <= 75){
+                    return result = "Medium Volume";
+
+                }else if(volume > 75 && volume < 100){
+                    return result =  "High Volume";
+
+                }else if(volume >= 100){
+                    return result = "Max Volume";
+
+                }else{
+                    return null;
                 }
             }
         }
     public static void main(String[] args) throws Exception{
-        
+        int volume = 25;
+        String result = Volume.getName(volume);
+        System.out.println(result);
     }
 }
