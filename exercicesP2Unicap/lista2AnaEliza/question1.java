0cb328f707e334d5d3f6dff70b5f7cfaca4e2e0f
Alef Rodrigues <alefsrodrigues03@gmail.com>
2022-09-19 11:46:19 +0000

    question1 list2 ana eliza complete

--- exercicesP2Unicap/lista2AnaEliza/question1.java
+++ exercicesP2Unicap/lista2AnaEliza/question1.java
@@ -9,6 +9,18 @@ public class question1 {
         sc.close();
         return vetor;
     }
+    static double[] getLucros(double[] vetGanhos, double[] vetGastos){
+        double[] vetLucros = new double[vetGanhos.length];
+        for(int i=0; i<vetGanhos.length;i++){
+            vetLucros[i] = vetGanhos[i] - vetGastos[i];
+        }
+        return vetLucros;
+    }
+    static void vetExibir(double[] vet1){
+        for(int i = 0;i<vet1.length;i++){
+            System.out.println(vet1[i]);
+        }
+    }
     public static void main(String[] args) throws Exception{
         Scanner sc = new Scanner(System.in);
         sc.close();
