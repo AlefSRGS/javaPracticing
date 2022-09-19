e2bb309bc7b9f0c10b685cc5d055302b87cd9dc3
Alef Rodrigues <alefsrodrigues03@gmail.com>
2022-09-19 12:06:36 +0000

    complete question 2 list 2 ana eliza

--- exercicesP2Unicap/lista2AnaEliza/question1and2.java
+++ exercicesP2Unicap/lista2AnaEliza/question1and2.java
@@ -1,6 +1,6 @@
 package exercicesP2Unicap.lista2AnaEliza;
 import java.util.Scanner;
-public class question1 {
+public class question1and2 {
     static double[] preencherVetor(double[] vetor){
         Scanner sc = new Scanner(System.in);
         for(int i=0; i<vetor.length;i++){
@@ -20,6 +20,13 @@ public class question1 {
         for(int i = 0;i<vet1.length;i++){
             System.out.println(vet1[i]);
         }
+    }
+    static double sumVetor(double[] vet1){
+        double vetorSomado = 0;
+        for(int i = 0; i<vet1.length;i++){
+            vetorSomado += vet1[i];
+        }
+        return vetorSomado;
     }
     public static void main(String[] args) throws Exception{
         Scanner sc = new Scanner(System.in);
