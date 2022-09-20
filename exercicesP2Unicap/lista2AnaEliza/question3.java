322bf86b5c5b58fad9d4f9b87e2ce2d79d02757c
Alef Rodrigues <alefsrodrigues03@gmail.com>
2022-09-20 15:03:03 +0000

    question 3 lis2 ana eliza complete

--- exercicesP2Unicap/lista2AnaEliza/question3.java
+++ exercicesP2Unicap/lista2AnaEliza/question3.java
@@ -1,9 +1,16 @@
 package exercicesP2Unicap.lista2AnaEliza;
 import java.util.Scanner;
 public class question3 {
+    static double[] calculatorMedia(double[] firstGrade, double[] secondGrade){
+        double[] vetorMedias = new double[firstGrade.length];
+        for(int i=0;i<vetorMedias.length-1;i++){
+            vetorMedias[i] = (firstGrade[i]*2 + secondGrade[i]*3)/5;
+        }
+        return vetorMedias;
+    }
     static void preencherVetores(String[] vetorNames, double[] vetorFirstGrade, double[] vetorSecondGrade, double[] vetorAverage, String[] vetorSituacion){
         Scanner sc = new Scanner(System.in);
-        for(int i =0; i<4;i++){
+        for(int i =0; i<2;i++){
             if(i==0){
                 for(int aux1=0;aux1<vetorNames.length-1;aux1++){
                     vetorNames[aux1] = sc.nextLine();
@@ -15,34 +22,37 @@ public class question3 {
                 }
             }
             if(i==2){
-                for(int aux1=0;aux1<vetorSecondGrade.length-1;aux1++){
-                    vetorSecondGrade[aux1] = sc.nextDouble();
+                for(int aux3=0;aux3<vetorSecondGrade.length-1;aux3++){
+                    vetorSecondGrade[aux3] = sc.nextDouble();
                 }
             }
-            if(i==3){
-                for(int aux1=0;aux1<vetorAverage.length-1;aux1++){
-                    vetorAverage[aux1] = sc.nextDouble();
-                }
-            }
-            if(i==4){
-                for(int aux1=0;aux1<vetorSituacion.length-1;aux1++){
-                    vetorSituacion[aux1] = sc.nextLine();
+            vetorAverage = calculatorMedia(vetorFirstGrade, vetorSecondGrade);
+            for(int aux4 =0;aux4<vetorAverage.length-1;aux4++){
+                if(vetorAverage[aux4]>=7){
+                    vetorSituacion[aux4]="aprovado";
+                }else if(vetorAverage[aux4]>=3){
+                    vetorSituacion[aux4]="recuperação";
+                }else{
+                    vetorSituacion[aux4]="reprovado";
                 }
             }
         }
         sc.close();
     }
-    static void calculatorMediaAndSituacion(double[] firstGrade, double[] secondGrade, double[] vetorMedias, String[] vetorSituacion){
-        for(int i=0;i<vetorMedias.length;i++){
-            vetorMedias[i] = (firstGrade[i]*2 + secondGrade[i]*3)/5;
-            if(vetorMedias[i] >= 7){
-                
-            }
-            }
+    static void exibirResult(String[] vetorNomes, double[] vetorMedias, String[] vetorSituação){
+        for(int i = 0; i<vetorNomes.length-1;i++){
+            System.out.printf("O aluno %s obteve média %f e está %s", vetorNomes, vetorMedias, vetorSituação);
         }
+    }
     public static void main(String[] args) throws Exception{
         Scanner sc = new Scanner(System.in);
-
+        String[] listNames =  new String[50];
+        double[] listFirstGrade =  new double[50];
+        double[] listSecondGrade =  new double[50];
+        double[] listAverage =  new double[50];
+        String[] listSituacion =  new String[50];
+        preencherVetores(listNames, listFirstGrade, listSecondGrade, listAverage, listSituacion);
+        exibirResult(listNames, listAverage, listSituacion);
         sc.close(); 
     }
 }
