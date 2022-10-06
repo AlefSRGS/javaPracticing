d2ae99048befae7561f53f69f89c1d2356524424
Alef Rodrigues <alefsrodrigues03@gmail.com>
2022-10-06 20:29:39 +0000

    unfinised question02

--- exercicesP2Unicap/list3Diego/src/main/java/br/unicap/p2/atividade03/part1/CompoundInterest.java
+++ exercicesP2Unicap/list3Diego/src/main/java/br/unicap/p2/atividade03/part1/CompoundInterest.java
@@ -1,23 +1,14 @@
 package br.unicap.p2.atividade03.part1;
 
 public class CompoundInterest {
-
-    public static double [][] calculate(double initialAmount, int numPeriods, double[] interestRates) {
-
-        double[][] taxesTable = new double[numPeriods + 1][interestRates.length];
-        
-        for (int m = 0; m < numPeriods + 1; m++) {
-            double amount = initialAmount;
-            for(int n = 0; n < interestRates.length; n++) {
-                initialAmount = amount;
-                taxesTable[m][n] = initialAmount;
-                for(int i = 0; i < interestRates.length; i++) {
-                    double tax = interestRates[i];
-                    initialAmount = initialAmount + (initialAmount * tax);
+        static double[][] calculate(double initialAmount, int numPeriods, double[] interestRates){
+            double[][] finalMatrix = new double[numPeriods+1][interestRates.length];
+            for(int i = 0; i<interestRates.length; i++){
+                for(int aux = 0; i<numPeriods+1;aux++){
+                    
                 }
             }
-        }
 
-        return taxesTable;
+            return finalMatrix;
+        }
     }
-}
