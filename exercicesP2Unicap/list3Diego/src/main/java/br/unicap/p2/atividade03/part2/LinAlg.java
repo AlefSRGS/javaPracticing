b2a9bb5434ace47f3e50ebfa3f3973a32afd9239
Alef Rodrigues <alefsrodrigues03@gmail.com>
2022-12-08 00:23:43 +0000

    .

--- exercicesP2Unicap/list3Diego/src/main/java/br/unicap/p2/atividade03/part2/LinAlg.java
+++ exercicesP2Unicap/list3Diego/src/main/java/br/unicap/p2/atividade03/part2/LinAlg.java
@@ -1,7 +1,7 @@
 package br.unicap.p2.atividade03.part2;
 
 public class LinAlg {
-    static double[][] matrixMultiplication(int a, double[][] A){
+    static double[][] matrixMultiplication(double a, double[][] A){
         for(int i =0;i<2;i++){
             for(int aux =0;aux<2;aux++){
                 A[i][aux] *= a;
@@ -11,7 +11,7 @@ public class LinAlg {
     }
 
     static double[][] matrixTranspose(double[][] A){
-        double[][] transposeA = new double[A[0].length][A.length]
+        double[][] transposeA = new double[A[0].length][A.length];
         for(int i =0;i<2;i++){
             for(int aux =0;aux<2;aux++){
                 transposeA[aux][i] = A[i][aux];
