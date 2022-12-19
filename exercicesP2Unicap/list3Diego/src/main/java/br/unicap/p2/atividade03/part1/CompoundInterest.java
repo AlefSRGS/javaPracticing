b5c7f47bbf60671161e85ddd1a8ea46a0e3d9722
Alef Rodrigues <alefsrodrigues03@gmail.com>
2022-12-19 19:36:18 +0000

    currection part 1 list 3 diego

--- exercicesP2Unicap/list3Diego/src/main/java/br/unicap/p2/atividade03/part1/CompoundInterest.java
+++ exercicesP2Unicap/list3Diego/src/main/java/br/unicap/p2/atividade03/part1/CompoundInterest.java
@@ -3,17 +3,20 @@ package br.unicap.p2.atividade03.part1;
 public class CompoundInterest {
         static double[][] calculate(double initialAmount, int numPeriods, double[] interestRates){
             double[][] finalMatrix = new double[numPeriods+1][interestRates.length];
-            for(int i = 0; i<interestRates.length; i++){
-                double atualMoney = initialAmount;
-                for(int aux = 0; i<numPeriods+1;aux++){
-                    if(aux == 0){
-                        finalMatrix[i][aux] = atualMoney;
+            for(int line =0; line< numPeriods+1;line++){
+                double atualMoney = 0;
+                int indexInterestRates = 0;
+                for(int column =0; column< interestRates.length;column++){
+                    if(line == 0){
+                        atualMoney = initialAmount;
+                        finalMatrix[line][column] = atualMoney;
                     }else{
-                        finalMatrix[i][aux] = atualMoney + (atualMoney*interestRates[i])/100;
+                        atualMoney = finalMatrix[line-1][column];
+                        finalMatrix[line][column] = atualMoney + atualMoney*interestRates[indexInterestRates];
+                        indexInterestRates++;
                     }
                 }
             }
-
             return finalMatrix;
         }
     }
