3850cabc1af461bd0f07c0fb60af84e3d7dbafc2
Alef Rodrigues <alefsrodrigues03@gmail.com>
2023-03-02 12:05:25 +0000

    +media Aritimetica

--- exercicesP2Unicap/exercIagoSala/aula0203.java
+++ exercicesP2Unicap/exercIagoSala/aula0203.java
@@ -13,9 +13,18 @@ public class aula0203{
             System.out.println(lista[i]);
         }
     }
+    static double mediaArray(double[] notas){
+        double media = 0;
+        for(int i=0;i<notas.length;i++){
+            media += notas[i];
+        }
+        media /= notas.length;
+        return media;
+    }
     public static void main(String[] args) {
         double listaArray[] = new double[10];
         preencherArray(listaArray);
         printList(listaArray);
+        System.out.println(mediaArray(listaArray));
     }
 }
