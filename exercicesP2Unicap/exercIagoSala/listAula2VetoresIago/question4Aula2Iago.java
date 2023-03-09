dc6b49d4d8fe4b9a455d50c28c5f5f23429af5cc
Alef Rodrigues <alefsrodrigues03@gmail.com>
2023-03-09 03:23:39 +0000

    complete question 4 class 2 iago

--- exercicesP2Unicap/exercIagoSala/listAula2VetoresIago/question4Aula2Iago.java
+++ exercicesP2Unicap/exercIagoSala/listAula2VetoresIago/question4Aula2Iago.java
@@ -1,3 +1,12 @@
+import question1Aula2Iago;
 public class question4Aula2Iago {
-    
+    static void printEvenIndex(double[] array){
+        for(int i=0; i<array.length;i+=2){
+            System.out.println(array[i]);
+        }
+    }
+    public static void main(String[] args) {
+        double[] array10elementosReais = new double[10];
+        question1Aula2Iago.readArrayDouble(array10elementosReais);
+    }
 }
